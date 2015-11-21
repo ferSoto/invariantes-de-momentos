bb5180bafbc20936236c948e3e415cfbdd0d6134
Fernando Soto <fersoto117@gmail.com>
2015-11-20 21:21:09 -0600

    Corrección de tipos de datos

--- Headers/Moments.h
+++ Headers/Moments.h
@@ -3,6 +3,7 @@
 */
 
 #include "Image.h"
+#include <tuple>
 
 #ifndef MOMENTS_H
 #define MOMENTS_H
@@ -19,11 +20,21 @@ public:
 	//Function to get central moment for scaling transformation.
 	static double getScalingMoment(Image img, int p, int q, double M_00);
 
-	//
+	//Function that return the first 3 invariants for rotation.
+	static std::tuple<double, double, double>getRotationMoments(Image img);
 
 private:
 	//Return M_00^((p+q)/2 + 1)
 	static double getM00some(double M_00, int p, int q);
+
+	//Rotation Invariant 1.
+	static double firstRI(Image img);
+
+	//Rotation Invariant 2.
+	static double secondRI(Image img);
+
+	//Rotation Invariant 3.
+	static double thirdRI(Image img);
 
 };
 #endif
--- src/Moments.cpp
+++ src/Moments.cpp
@@ -5,7 +5,7 @@
 #include "../Headers/Moments.h"
 #include "../Headers/Image.h"
 #include <cmath>
-#include <iostream>
+#include <tuple>
 
 typedef unsigned int ui;
 
@@ -18,7 +18,7 @@ double Moments::centralMoment(Image img, int p, int q){
 	double Mpq = 0;
 	for(ui xi = 0; xi < img.getHeigh(); xi++)
 		for(ui yj = 0; yj < img.getWidth(); yj++)
-		if(img(xi,yj) == 1)				 
+		if(img(xi,yj))				 
 			Mpq += (pow((double)xi-(double)img.getXmc(),(double)p)
 				*pow((double)yj-(double)img.getYmc(),(double)q));
 	return Mpq;
@@ -49,4 +49,34 @@ double Moments::getM00some(double M_00, int p, int q){
 */
 double Moments::getScalingMoment(Image img, int p, int q, double M_00){
 	return (double)centralMoment(img, p, q)/(double)getM00some(M_00, p, q);
+}
+
+/**
+* Return M20 + M02
+*/
+double Moments::firstRI(Image img){
+	return centralMoment(img, 0, 2) + centralMoment(img, 2, 0);
+}
+
+/**
+* Return (M20 - M02)^2 + 4*(M11)^2
+*/
+double Moments::secondRI(Image img){
+	return pow(centralMoment(img, 2, 0) - centralMoment(img, 0, 2), 2) 
+		+ 4*pow(centralMoment(img, 1, 1), 2);
+}
+
+/**
+* Return (M30 - 3*M12)^2 + (3*M21 - M03)^2
+*/
+double Moments::thirdRI(Image img){
+	return pow(centralMoment(img, 3, 0) - 3*centralMoment(img, 1, 2), 2) 
+		+ pow(3*centralMoment(img, 2, 1) - centralMoment(img, 0, 3), 2);
+}
+
+/**
+* Receives an Image and returns the first 3 rotation invariant moments.
+*/
+std::tuple<double, double, double> Moments::getRotationMoments(Image img){
+	return std::make_tuple(firstRI(img), secondRI(img), thirdRI(img));
 }
